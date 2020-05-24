0e68fcb8c8fe3db0fd78469a820b59b9a98707e3
sangchenchen <sangchenchen@glaway.com>
2020-05-24 19:08:08 +0800

    test

--- Module01/src/com/scc/test/Sone.java
+++ Module01/src/com/scc/test/Sone.java
@@ -11,6 +11,6 @@ package com.scc.test;
  */
 public class Sone {
     public static void main(String[] args) {
-        System.out.println("dsadsa");
+        System.out.println("dsadsadsadsadsdsa");
     }
 }
